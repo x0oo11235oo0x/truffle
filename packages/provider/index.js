2c8137fbf9479bba5f1fd890c9e24b8668914c02
cds-amal <cds.sudama@gmail.com>
2022-04-05 12:23:06 -0400

    provider: memoize the provider instance
    - use closure for storage
    - export lifecycle mgmt api for cleanup needs
    - cleanup old provider when using `create` api

--- packages/provider/index.js
+++ packages/provider/index.js
@@ -4,33 +4,43 @@ const { createInterfaceAdapter } = require("@truffle/interface-adapter");
 const wrapper = require("./wrapper");
 const DEFAULT_NETWORK_CHECK_TIMEOUT = 5000;
 
+let _providerMemo = null;
+
 module.exports = {
   wrap: function (provider, options) {
     return wrapper.wrap(provider, options);
   },
 
+  resetMemoProvider: function () {
+    _providerMemo = null;
+  },
+
   create: function (options) {
+    //first clean up old provider
+    this.resetMemoProvider();
+
     const provider = this.getProvider(options);
     return this.wrap(provider, options);
   },
 
   getProvider: function (options) {
-    let provider;
+    if (_providerMemo) return _providerMemo;
+
     if (options.provider && typeof options.provider === "function") {
-      provider = options.provider();
+      _providerMemo = options.provider();
     } else if (options.provider) {
-      provider = options.provider;
+      _providerMemo = options.provider;
     } else if (options.websockets || /^wss?:\/\//.test(options.url)) {
-      provider = new Web3.providers.WebsocketProvider(
+      _providerMemo = new Web3.providers.WebsocketProvider(
         options.url || "ws://" + options.host + ":" + options.port
       );
     } else {
-      provider = new Web3.providers.HttpProvider(
+      _providerMemo = new Web3.providers.HttpProvider(
         options.url || `http://${options.host}:${options.port}`,
         { keepAlive: false }
       );
     }
-    return provider;
+    return _providerMemo;
   },
 
   testConnection: function (options) {
@@ -49,17 +59,18 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const noResponseFromNetworkCall = setTimeout(() => {
         let errorMessage =
-          "There was a timeout while attempting to connect to the network at " + host +
+          "There was a timeout while attempting to connect to the network at " +
+          host +
           ".\n       Check to see that your provider is valid." +
           "\n       If you have a slow internet connection, try configuring a longer " +
           "timeout in your Truffle config. Use the " +
           "networks[networkName].networkCheckTimeout property to do this.";
 
-          if (network === "dashboard") {
-            errorMessage +=
-              "\n       Also make sure that your Truffle Dashboard browser " +
-              "tab is open and connected to MetaMask.";
-          }
+        if (network === "dashboard") {
+          errorMessage +=
+            "\n       Also make sure that your Truffle Dashboard browser " +
+            "tab is open and connected to MetaMask.";
+        }
 
         throw new Error(errorMessage);
       }, networkCheckTimeout);
@@ -75,7 +86,9 @@ module.exports = {
           } catch (error) {
             console.log(
               "> Something went wrong while attempting to connect to the " +
-                "network at " + host + ". Check your network configuration."
+                "network at " +
+                host +
+                ". Check your network configuration."
             );
             clearTimeout(noResponseFromNetworkCall);
             clearTimeout(networkCheck);
@@ -86,5 +99,5 @@ module.exports = {
         }, networkCheckDelay);
       })();
     });
-  },
+  }
 };
