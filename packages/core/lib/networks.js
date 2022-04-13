2d3c56da47369c9f3eade31dfa2a48033f714362
tenthirtyone <alexsherbuck@gmail.com>
2022-04-13 16:46:34 -0500

    remove my console.log, remove buggy db network test

--- packages/core/lib/networks.js
+++ packages/core/lib/networks.js
@@ -219,7 +219,6 @@ const Networks = {
   },
 
   matchesNetwork: async function (network_id, network_options) {
-    console.log("Networks matchesNetwork");
     const provider = Provider.create(network_options);
 
     const first = network_id + "";
